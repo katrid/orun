180af024271a17734c661924cc052d10cc270a63
alexandre <alexandre@katrid.com>
2019-07-18 14:25:32 -0300

    Initial commit

--- orun/addons/base/management/commands/changepassword.py
+++ orun/addons/base/management/commands/changepassword.py
@@ -21,7 +21,7 @@ def command(database, username, **options):
     if not options.get('password'):
         password = commands.prompt('New Password', hide_input=True)
     else:
-        password = options['password']
+        password = options['password'][0]
     user = app['auth.user']
     u = user.objects.filter(user.c.username == username).one()
     u.set_password(password)
