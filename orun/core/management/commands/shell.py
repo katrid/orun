342b1e01fb5241bbd6f76a94ecea5b20f8df2aa9
alexandre <alexandre@katrid.com>
2019-07-25 00:37:06 -0300

    Initial commit

--- orun/core/management/commands/shell.py
+++ orun/core/management/commands/shell.py
@@ -1,10 +1,12 @@
 import os
 import sys
-import click
+
+from orun.db import DEFAULT_DB_ALIAS
+from orun.core.management import commands
 
 
-@click.command('shell')
-def command():
+@commands.command('shell')
+def command(database, **options):
     """Runs an interactive Python shell in the context of a given
     Flask application.  The application will populate the default
     namespace of this shell according to it's configuration.
@@ -12,6 +14,13 @@ def command():
     This is useful for executing small snippets of management code
     without having to manually configuring the application.
     """
+    if database:
+        from orun import app
+        from sqlalchemy.engine.url import make_url
+        url = make_url(app.settings['DATABASES'][DEFAULT_DB_ALIAS]['ENGINE'])
+        url.database = database
+        app.settings['DATABASES'][DEFAULT_DB_ALIAS]['ENGINE'] = str(url)
+
     import code
     from flask.globals import _app_ctx_stack
     app = _app_ctx_stack.top.app
