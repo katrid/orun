6dbd71fb89dd07831890ccaec1b7b89ec6850253
alexandre <alexandre@katrid.com>
2019-10-11 17:22:19 -0300

    Initial commit

--- orun/addons/web/views/reports.py
+++ orun/addons/web/views/reports.py
@@ -9,6 +9,9 @@ class Reports(BaseView):
     @route('/model/choices/', methods=['POST'])
     def model_choices(self):
         data = request.json
-        print(data)
         model = app[data['model']]
+        model_field = data['kwargs'].get('model_field')
+        if model_field:
+            model_field = model._meta.fields[model_field]
+            print(model_field)
         return jsonify(model.search_name(name=data.get('q'), count=data.get('count'), page=data.get('page')))
