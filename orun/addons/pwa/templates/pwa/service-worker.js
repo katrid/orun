c4de5edcc69dd03e61388bac7be9de6c9c7f9c91
alexandre <alexandre@katrid.com>
2020-01-09 06:47:47 -0300

    Initial commit

--- orun/addons/pwa/templates/pwa/service-worker.js
+++ orun/addons/pwa/templates/pwa/service-worker.js
@@ -50,7 +50,7 @@ self.addEventListener("fetch", function(event) {
 
   let request = event.request.clone();
 
-  if (event.request.method === 'POST')
+  if (event.request.method !== 'POST')
   event.respondWith(
     // try to get fresh data
     fetch(event.request)
