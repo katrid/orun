869fbd6657e95d5169b5ab0618d0a9df06d8b431
alexandre <alexandre@katrid.com>
2020-03-14 09:43:06 -0300

    Initial commit

--- orun/db/backends/mssql/operations.py
+++ orun/db/backends/mssql/operations.py
@@ -21,6 +21,11 @@ class DatabaseOperations(BaseDatabaseOperations):
             return name
         return '"%s"' % name
 
+    def bulk_insert_sql(self, fields, placeholder_rows):
+        placeholder_rows_sql = (", ".join(row) for row in placeholder_rows)
+        values_sql = ", ".join("(%s)" % sql for sql in placeholder_rows_sql)
+        return "VALUES " + values_sql
+
     def adapt_datetimefield_value(self, value):
         """
         Transform a datetime value to an object compatible with what is expected
