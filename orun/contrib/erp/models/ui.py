42553eba4756f75426c22840114f394bcd08091e
alexandre <alexandre@katrid.com>
2020-04-26 14:37:55 -0300

    Initial commit

--- orun/contrib/erp/models/ui.py
+++ orun/contrib/erp/models/ui.py
@@ -91,7 +91,7 @@ class View(models.Model):
         pos = element.attrib.get('position')
         expr = element.attrib.get('expr')
         target = source
-        logger.critical('xpath %s' % self.template_name)
+        logger.critical('xpath %s %s' % (expr, self.template_name))
         if expr:
             target = target.xpath(expr)[0]
         if pos == 'append':
