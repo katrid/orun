4e903f6f7a723b6bbde87cefeb6e4af9e00107ef
alexandre <alexandre@katrid.com>
2020-09-07 15:07:46 -0300

    Initial commit

--- orun/db/backends/utils.py
+++ orun/db/backends/utils.py
@@ -234,11 +234,12 @@ def format_number(value, max_digits, decimal_places):
     context = decimal.getcontext().copy()
     if max_digits is not None:
         context.prec = max_digits
-    if decimal_places is not None and isinstance(value, decimal.Decimal):
-        value = value.quantize(decimal.Decimal(1).scaleb(-decimal_places), context=context)
-    else:
-        context.traps[decimal.Rounded] = 1
-        value = context.create_decimal(value)
+    if isinstance(value, decimal.Decimal):
+        if decimal_places is not None:
+            value = value.quantize(decimal.Decimal(1).scaleb(-decimal_places), context=context)
+        else:
+            context.traps[decimal.Rounded] = 1
+            value = context.create_decimal(value)
     return "{:f}".format(value)
 
 
