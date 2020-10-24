66a5f7826926f406f80e043be25d974389f0338c
alexandre <alexandre@katrid.com>
2020-10-24 18:34:58 -0300

    Initial commit

--- orun/contrib/pwa/templates/pwa/service-worker.js
+++ orun/contrib/pwa/templates/pwa/service-worker.js
@@ -1,89 +1,60 @@
-let CACHE_NAME = 'orun-v2';
+//{% block version %}
+let CACHE_NAME = 'orun-v1';
 const DB_NAME = 'orun-db-cache';
-
-self.importScripts(['/static/web/assets/js/alasql.min.js']);
-
+//{% endblock %}
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll([
-        "/pwa/",
-        "/static/web/assets/js/toastr/toastr.min.css",
-        "/static/web/assets/css/bootstrap.min.css",
-        "/static/web/assets/css/fontawesome.css",
-        "/static/web/api/theme/style.css",
-        "/static/web/assets/js/datepicker/bootstrap-datetimepicker.min.css",
-        "/static/web/assets/js/select2/select2.css",
-        "/static/web/assets/js/c3/c3.min.css",
-        "/static/web/assets/css/animate.css",
-        "/static/web/assets/js/jquery.min.js",
+        //{% block cache %}
+        "/static/admin/assets/js/toastr/toastr.min.css",
+        "/static/admin/assets/css/bootstrap.min.css",
+        "/static/admin/assets/css/fontawesome.css",
+        "/static/admin/api/theme/style.css",
+        "/static/admin/assets/js/datepicker/bootstrap-datetimepicker.min.css",
+        "/static/admin/assets/js/select2/select2.css",
+        "/static/admin/assets/js/c3/c3.min.css",
+        "/static/admin/assets/css/animate.css",
+        "/static/admin/assets/js/sweetalert2.css",
+        "/static/admin/katrid/style.css",
 
-        "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js",
-        "/static/web/assets/js/moment/moment.js",
-        "/static/web/assets/js/moment/moment-duration-format.js",
-        "/static/web/assets/js/moment/pt-br.js",
-        "/static/web/assets/js/nunjucks.js",
-        "/static/web/assets/js/select2/select2.js",
-        "/static/web/assets/js/popper.js",
-        "/static/web/assets/js/bootstrap.min.js",
-        "/static/web/assets/js/angular/angular.min.js",
-        "/static/web/assets/js/angular/angular-sanitize.min.js",
-        "/static/web/assets/js/hotkeys/hotkeys.min.js",
-        "/static/web/assets/js/jquery.inputmask.bundle.js",
-        "/static/web/assets/js/datepicker/bootstrap-datetimepicker.js",
-        "/static/web/assets/js/alasql.min.js",
-        "/static/web/assets/js/toastr/toastr.min.js",
-        "/static/web/assets/js/underscore-min.js",
-        "/static/web/assets/js/c3/d3.v3.min.js",
-        "/static/web/assets/js/c3/c3.min.js",
+        "/static/admin/assets/js/jquery.min.js",
+        "/static/admin/assets/js/jquery-ui.min.js",
+        "/static/admin/assets/js/moment/moment.js",
+        "/static/admin/assets/js/moment/moment-duration-format.js",
+        "/static/admin/assets/js/moment/pt-br.js",
+        "/static/admin/assets/js/nunjucks.js",
+        "/static/admin/assets/js/select2/select2.js",
+        "/static/admin/assets/js/popper.js",
+        "/static/admin/assets/js/bootstrap.min.js",
+        "/static/admin/assets/js/angular/angular.min.js",
+        "/static/admin/assets/js/angular/angular-sanitize.min.js",
+        "/static/admin/assets/js/hotkeys/hotkeys.min.js",
+        "/static/admin/assets/js/jquery.inputmask.bundle.js",
+        "/static/admin/assets/js/datepicker/bootstrap-datetimepicker.js",
+        "/static/admin/assets/js/toastr/toastr.min.js",
+        "/static/admin/assets/js/underscore-min.js",
+        "/static/admin/assets/js/c3/d3.v3.min.js",
+        "/static/admin/assets/js/c3/c3.min.js",
 
-        "/static/web/api/1.7/katrid.full.min.js",
+        "/static/admin/katrid/katrid.js",
+
+        //{% if LANGUAGE_CODE != 'en-us' %}
+        "/static/admin/assets/js/angular/i18n/angular-locale_{{ LANGUAGE_CODE }}.js",
+        "/static/admin/assets/js/select2/select2_locale_{{ LANGUAGE_CODE }}.min.js",
+        //{% endif %}
+        //{% endblock %}
       ]);
     })
   )
 });
 
 
-self.addEventListener("fetch", function(event) {
-
-  let request = event.request.clone();
-
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    // try to get fresh data
-    fetch(event.request)
-      // put a fresh copy on the cache
-      // .then(function(res) {
-      //   caches.open(CACHE_NAME)
-      //     .then(function(cache) {
-      //       cachePut(cache, request, res);
-      //     });
-      //   return res;
-      // })
-      // falling back to the cache
-      .catch(function() {
-        console.log(event.request);
-        return caches.match(event.request);
-      })
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
-
 });
-
-
-
-// put JSON RPC on the cache
-function cachePut(cache, request, response) {
-  if (request.method === 'POST') {
-    // get hashable key
-    request.text().then(function(data) {
-      let hash = request.url + data;
-
-    });
-  } else if (!response.bodyUsed)
-    cache.put(request, response);
-}
-
-function cacheMatch(cache, key) {
-  return cache.match(key);
-}
