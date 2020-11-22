2935f6662d164859b55c461c4cdc9e8cf4072820
alexandre <alexandre@katrid.com>
2020-11-22 11:38:04 -0300

    Initial commit

--- orun/core/serializers/csv.py
+++ orun/core/serializers/csv.py
@@ -7,6 +7,7 @@ from functools import partial
 
 from orun.core.serializers import python, base
 from orun.apps import apps
+from orun.db import connection
 
 
 class Deserializer(base.Deserializer):
@@ -22,17 +23,31 @@ class Deserializer(base.Deserializer):
                 delimiter = ','
         else:
             delimiter = '\t'
+
         cols = h.split(delimiter)
         reader = csv.DictReader(stream_or_string, fieldnames=cols, delimiter=delimiter)
         cols = reader.fieldnames
         model_name = self.path.name.rsplit('.', 1)[0]
         model = apps[model_name]
-        # mandatory fields for csv deserializer
-        i = 0
+
         try:
-            for i, obj in enumerate(python.Deserializer(reader, model=model, fields=cols)):
-                obj.save(using=self.database)
-        except Exception as e:
-            print('Error at line:', i)
-            raise
-        self.postpone = [partial(self.reset_sequence, model)]
+
+            # set identity_insert on mssql database table
+            if connection.vendor == 'mssql':
+                cur = connection.cursor()
+                cur.execute('''set identity_insert %s on''' % model._meta.db_table)
+
+            # mandatory fields for csv deserializer
+            i = 0
+            try:
+                for i, obj in enumerate(python.Deserializer(reader, model=model, fields=cols)):
+                    obj.save(using=self.database)
+            except Exception as e:
+                print('Error at line:', i)
+                raise
+            self.postpone = [partial(self.reset_sequence, model)]
+        finally:
+            # set identity_insert on mssql database table
+            if connection.vendor == 'mssql':
+                cur = connection.cursor()
+                cur.execute('''set identity_insert %s off''' % model._meta.db_table)
