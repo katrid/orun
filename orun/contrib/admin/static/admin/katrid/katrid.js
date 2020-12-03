b2e99357dba997b586915355e1cccaa38e5fffb9
alexandre <alexandre@katrid.com>
2020-12-03 17:08:02 -0300

    Initial commit

--- orun/contrib/admin/static/admin/katrid/katrid.js
+++ orun/contrib/admin/static/admin/katrid/katrid.js
@@ -5104,7 +5104,17 @@ var Katrid;
                     });
                 }
                 copyClick() {
-                    navigator.clipboard.writeText(Katrid.UI.Utils.tableToText(this.table));
+                    if (typeof (navigator.clipboard) == 'undefined') {
+                        let textArea = document.createElement("textarea");
+                        textArea.value = Katrid.UI.Utils.tableToText(this.table);
+                        textArea.style.position = "fixed";
+                        document.body.appendChild(textArea);
+                        textArea.focus();
+                        textArea.select();
+                        document.body.removeChild(textArea);
+                    }
+                    else
+                        navigator.clipboard.writeText(Katrid.UI.Utils.tableToText(this.table));
                 }
                 deleteRow() {
                     console.log('delete selected rorw');
