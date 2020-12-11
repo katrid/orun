39ef0743f26e7b3b5d6111c9a6719eeb3da19bde
alexandre <alexandre@katrid.com>
2020-12-11 12:00:15 -0300

    Initial commit

--- orun/contrib/pwa/models.py
+++ orun/contrib/pwa/models.py
@@ -12,4 +12,12 @@ class PwaSync(models.Model):
     @classmethod
     def sync(cls, service: str, values: dict):
         model = apps[service]
+        for k, v in values.items():
+            if isinstance(v, list):
+                field = model._meta.fields[k]
+                if field.one_to_many:
+                    new_val = []
+                    for obj in v:
+                        new_val.append({'action': 'CREATE', 'values': {k: v for k, v in obj.items() if not k.startswith('$')}})
+                    values[k] = new_val
         return model.write(values)
