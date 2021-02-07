5c48009f0251f96954ea18887f620c1b40ac59a0
alexandre <alexandre@katrid.com>
2021-02-06 21:20:33 -0300

    Initial commit

--- orun/contrib/mail/apps.py
+++ orun/contrib/mail/apps.py
@@ -6,5 +6,5 @@ class MailConfig(AppConfig):
     verbose_name = 'Basic ERP Module'
     auto_install = True
     default_language = 'en-us'
-    fixtures = ['modules.xml', 'views.xml', 'actions.xml', 'menu.xml', 'currency.xml', 'country.xml', 'partner.xml']
+    fixtures = ['mail.xml']
     dependencies = ['orun.contrib.admin']
--- orun/contrib/mail/models.py
+++ orun/contrib/mail/models.py
@@ -1,3 +1,4 @@
+from typing import Optional, List
 from orun.db import models
 from orun.utils.translation import gettext_lazy as _
 
@@ -20,4 +21,31 @@ class MailServer(models.Model):
     smtp_debug = models.BooleanField(default=False)
 
     class Meta:
-        name = 'ir.mail.server'
+        name = 'mail.server'
+
+
+class Channel(models.Model):
+    name = models.CharField(null=False, unique=True)
+    channel_type = models.SelectionField(
+        (
+            ('chat', 'Chat'),
+            ('channel', 'Channel'),
+        ), default='channel', label=_('Channel Type'),
+    )
+    description = models.TextField()
+    access = models.SelectionField(
+        (
+            ('public', 'Everyone'),
+            ('private', 'Invited people only'),
+            ('groups', 'Selected groups of users')
+        ), default='groups',
+    )
+    partners = models.ManyToManyField('res.partner')
+    groups = models.ManyToManyField('auth.group', label=_('Groups'))
+    moderate = models.BooleanField()
+
+    class Meta:
+        name = 'mail.channel'
+
+    def send_to_partner(self, partner, message: str, subject: Optional[str]=None, attachments: Optional[List]=None):
+        pass
