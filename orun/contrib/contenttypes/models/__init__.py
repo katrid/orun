8a1312fab67a915e93ddc71c715247934aa02576
alexandre <alexandre@katrid.com>
2021-08-25 15:41:30 -0300

    Initial commit

--- orun/contrib/contenttypes/models/__init__.py
+++ orun/contrib/contenttypes/models/__init__.py
@@ -1,3 +1,7 @@
 from .models import ContentType, ContentTypeManager
 from .objects import Object, ObjectManager, Association
 from .attachment import Attachment
+
+
+def ref(key: str):
+    return Object.objects.filter(name=key).only('object_id').first().object_id
