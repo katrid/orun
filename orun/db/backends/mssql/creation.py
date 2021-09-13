fe796b228fd282d7534b5c0a91e7572f1bc214ab
alexandre <alexandre@katrid.com>
2021-09-13 15:47:23 -0300

    Initial commit

--- orun/db/backends/mssql/creation.py
+++ orun/db/backends/mssql/creation.py
@@ -13,3 +13,16 @@ class DatabaseCreation(BaseDatabaseCreation):
         cursor.execute('SELECT 1 FROM sysdatabases WHERE name = %s', [strip_quotes(database_name)])
         return cursor.fetchone() is not None
 
+
+    def _destroy_test_db(self, test_database_name, verbosity):
+        """
+        Internal implementation - remove the test db tables.
+        """
+        # Remove the test database to clean up after
+        # ourselves. Connect to the previous database (not the test database)
+        # to do so, because it's not allowed to delete a database while being
+        # connected to it.
+        self.connection.cursor().execute('USE master')
+        with self.connection._nodb_connection.cursor() as cursor:
+            cursor.execute("DROP DATABASE %s"
+                           % self.connection.ops.quote_name(test_database_name))
