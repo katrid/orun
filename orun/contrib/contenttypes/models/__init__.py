bbcade0d546aff90c2b5cbf3c7d5d0cfff808337
alexandre <alexandre@katrid.com>
2021-11-09 15:21:33 -0300

    Initial commit

--- orun/contrib/contenttypes/models/__init__.py
+++ orun/contrib/contenttypes/models/__init__.py
@@ -4,4 +4,7 @@ from .attachment import Attachment
 
 
 def ref(key: str):
-    return Object.objects.filter(name=key).only('object_id').first().object_id
+    obj = Object.objects.filter(name=key).only('object_id').first()
+    if not obj:
+        raise Object.DoesNotExist(f'Object not found "{key}"')
+    return obj.object_id
