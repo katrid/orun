86f885af8b2b81bc8f3a09c7c1563048208e7b23
alexandre <alexandre@katrid.com>
2022-05-11 13:40:16 -0300

    Vue3 Update

--- orun/api.py
+++ orun/api.py
@@ -103,6 +103,7 @@ def onchange(*fields):
     def decorator(fn):
 
         def contribute_to_class(flds, cls, name: str):
+            setattr(cls, name, fn)
             if not isinstance(flds, (tuple, list)):
                 flds = [flds]
             for field in flds:
@@ -111,7 +112,6 @@ def onchange(*fields):
                 lst = cls._meta.field_change_event[field]
                 if fn not in lst:
                     lst.append(fn)
-            setattr(cls, name, fn)
 
         fn.contribute_to_class = partial(contribute_to_class, fields)
         return fn
