cfa175ce52fd1a404ee13b627c1e1603c2af88bf
alexandre <alexandre@katrid.com>
2022-09-12 18:36:36 -0300

    Parameters

--- orun/contrib/admin/test.py
+++ orun/contrib/admin/test.py
@@ -4,4 +4,4 @@ from orun.test import Client
 class ClientRPC(Client):
     def rpc(self, service: str, method: str, data: dict):
         """Emulate the JSONRPC write method"""
-        return self.post(f'/api/rpc/{service}/{method}/', {'params': {'kwargs': {'data': data}}}, content_type='application/json')
+        return self.post(f'/api/rpc/{service}/{method}/', {'params': data}, content_type='application/json')
