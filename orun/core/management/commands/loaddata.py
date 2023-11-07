6c9fb91c6df0f06ec0b17e2f30b3d3a2388a76d0
alexandredias <alexandre@katrid.com>
2023-11-07 12:10:46 -0300

    Fixed admin.py loading fixture order

--- orun/core/management/commands/loaddata.py
+++ orun/core/management/commands/loaddata.py
@@ -13,11 +13,9 @@ from orun.core.management.base import BaseCommand, CommandError
 
 def _load_from_module(module):
     from orun.contrib.contenttypes.models import Registrable
-    for attr in dir(module):
-        if attr.startswith('_'):
-            # cannot register protected members
-            continue
-        member = getattr(module, attr)
+    members = [(inspect.getsourcelines(m)[1], m) for _, m in inspect.getmembers(module, inspect.isclass) if not m.__name__.startswith('_')]
+    members.sort(key=lambda m: m[0])
+    for member in members:
         if inspect.ismodule(member):
             if member.__name__.startswith(f'{module.__name__}.'):
                 _load_from_module(member)
