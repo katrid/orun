bfaecc6c26398298c28bb2197366cf9815467b36
alexandredias <alexandre@katrid.com>
2024-05-17 11:04:32 -0300

    select for update bug fix

--- orun/db/backends/base/operations.py
+++ orun/db/backends/base/operations.py
@@ -205,14 +205,15 @@ class BaseDatabaseOperations:
         """
         return []
 
-    def for_update_sql(self, nowait=False, skip_locked=False, of=()):
+    def for_update_sql(self, nowait=False, skip_locked=False, of=(), no_key=False):
         """
         Return the FOR UPDATE SQL clause to lock rows for an update operation.
         """
-        return 'FOR UPDATE%s%s%s' % (
-            ' OF %s' % ', '.join(of) if of else '',
-            ' NOWAIT' if nowait else '',
-            ' SKIP LOCKED' if skip_locked else '',
+        return "FOR%s UPDATE%s%s%s" % (
+            " NO KEY" if no_key else "",
+            " OF %s" % ", ".join(of) if of else "",
+            " NOWAIT" if nowait else "",
+            " SKIP LOCKED" if skip_locked else "",
         )
 
     def _get_limit_offset_params(self, low_mark, high_mark):
