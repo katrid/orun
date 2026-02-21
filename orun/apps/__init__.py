9895f90348820850d64c940db031e557404b2bd1
alexandredias <alexandre@katrid.com>
2026-02-21 00:34:20 -0300

    simplify the apps registry test case

--- orun/apps/__init__.py
+++ orun/apps/__init__.py
@@ -2,6 +2,10 @@ import os
 
 from .config import AppConfig
 from .registry import apps, Registry
-from .app import WebApplication
+# from .app import WebApplication
 
-__all__ = ['AppConfig', 'apps', 'WebApplication', 'Registry']
+__all__ = [
+    'AppConfig', 'apps',
+    # 'WebApplication',
+    'Registry'
+]
